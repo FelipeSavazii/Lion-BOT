a781ca3f8ffd3a8c16bd1099fd0aa19a43cc3607
FelipeSavazi <90295858+FelipeSavazii@users.noreply.github.com>
2021-09-25 17:17:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import os
 import re
 
+import aiohttp
 import discord
 from discord.ext import commands
 
@@ -39,6 +40,7 @@ async def on_ready():
     print('Online')
     global startTime
     startTime = time.time()
+    bot.remove_command('help')
     embed = discord.Embed(title="Estou online!", description=f" ", color=color)
     await channel.send(embed=embed)
     while True:
@@ -70,7 +72,7 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    embed = discord.Embed(title=" ", description=f"🏓 **Minha latência é {int(bot.latency * 1000)}ms**", color=color)
+    embed = discord.Embed(title="🏓 PING", description=f"**Minha latência é {int(bot.latency * 1000)}ms**", color=color)
     embed.set_footer(text=f"Executado por {ctx.author}")
     await ctx.send(embed=embed)
 
@@ -78,8 +80,32 @@ async def ping(ctx):
 @bot.command()
 async def uptime(ctx):
     uptime = str(datetime.timedelta(seconds=int(round(time.time() - startTime))))
-    embed = discord.Embed(title=" ", description=f"⏲️ **Estou acordado há {uptime}**", color=color)
+    embed = discord.Embed(title="⏲️ UPTIME", description=f"**Estou acordado há {uptime}**", color=color)
     embed.set_footer(text=f"Executado por {ctx.author}")
+    await ctx.send(embed=embed)
+    
+@bot.command()
+@commands.is_owner()
+async def reload(ctx):
+    embed = discord.Embed(title="🔄 RELOAD", color=color)  
+    for root, _, files in os.walk('commands'):
+        for file in files:
+            path = os.path.join(root, file)
+
+            if not os.path.isfile(path):
+                continue
+
+            path, ext = os.path.splitext(path)
+            if ext != '.py':
+                continue
+
+            extension = re.sub('\\\\|\/', '.', path)
+            try:
+                bot.reload_extension(extension) 
+                embed.add_field(name='\u200b', value=f'Carregada com sucesso: {extension}.', inline=False)
+            except Exception as e:
+                embed.add_field(name='\u200b', value=f'Falha ao carregar: {extension}. ({e})', inline=False)
+            embed.set_footer(text=f"Executado por {ctx.author}")
     await ctx.send(embed=embed)
 
 bot.run(TOKEN)
